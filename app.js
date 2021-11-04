72f4ac3e3115865f19a9a001264d5097fd8f79f8
Pablo Rex <pablorex2012@gmail.com>
2021-11-04 11:18:53 -0300

    agregado de crud

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const { connect } = require('./routes');
-
+var cors = require('cors');
 
 require('dotenv').config();
 app.use(express.urlencoded({ extended: false }));
@@ -14,9 +14,8 @@ app.set('view engine', 'hbs')
 app.use(express.static("public"));
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname,'public')));
 hbs.registerPartials(__dirname + "/views/partials/");
 
@@ -26,6 +25,16 @@ hbs.registerPartials(__dirname + "/views/partials/");
 app.listen(port, () => {
     console.log(`Usando el puerto http://localhost:${port}`)
 });
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //conexion a base de datos
 
@@ -55,7 +64,7 @@ app.get('/index', (req, res) => {
     res.render('index')
 })
 app.get('/alquileres', (req, res) => {
-    let sql = "SELECT * FROM propiedades WHERE estado_inm=1";
+    let sql = "SELECT * FROM propiedades WHERE estado_inm='Alquiler'";
     let query = conn.query(sql, (err, results) => {
         if (err) throw err;
         res.render('alquileres', {
@@ -63,6 +72,23 @@ app.get('/alquileres', (req, res) => {
         });
     });
 })
+
+//Crear nuevo registro
+
+app.post('/propiedades', (req, res) => {
+    let data = { precio: req.body.precio, supcub: req.body.supcub, suptot: req.body.suptot, dormi: req.body.dormi, descripcion: req.body.descripcion, estado_inm: req.body.estado_inm };
+    let sql = "INSERT INTO propiedades SET?";
+    conn.query(sql, data, function(error, results) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+
+//Trae todas las fotos de galeria
 
 app.get('/galeria', (req, res) => {
     let sql = "SELECT * FROM galeria";
@@ -74,14 +100,63 @@ app.get('/galeria', (req, res) => {
     });
 })
 
-app.get('/a201', (req, res) => {
+//Traer todos los registros
+app.get('/propiedades', (req, res) => {
+    let sql = "SELECT * FROM propiedades";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(results)
+            // res.render('admin', {
+            //     results
+            // });
+    });
+})
 
-        res.render('a201')
-    })
-    // app.get('/success', (req, res) => {
+//editar
+app.put('/propiedades/:id', (req, res) => {
+    let id = req.params.id;
+    let precio = req.body.precio;
+    let supcub = req.body.supcub;
+    let suptot = req.body.suptot;
+    let dormi = req.body.dormi;
+    let descripcion = req.body.descripcion;
+    let estado_inm = req.body.estado_inm;
+    let sql = "UPDATE propiedades SET precio=?, supcub=?, suptot=?, dormi=?, descripcion=?, estado_inm=? WHERE id=?";
+    conn.query(sql, [precio, supcub, suptot, dormi, descripcion, estado_inm, id], function(error, results) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(results);
+        }
+    });
+});
 
-//     res.render('success')
-// })
+//Eliminar
+
+app.delete('/propiedades/:id', (req, res) => {
+    conn.query('DELETE FROM propiedades WHERE id=?', [req.params.id], function(error, filas) {
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    });
+});
+
+//Agregar
+
+app.post
+
+app.get('/admin', (req, res) => {
+    let sql = "SELECT * FROM propiedades";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        // res.send(results)
+        res.render('admin', {
+            results
+        });
+    });
+})
 
 
 // app.get('/contactos', (req, res) => {
